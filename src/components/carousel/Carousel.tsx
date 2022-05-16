dcb51ca68bc8439c537d402ffe19e89de033f2dc
Yeatsi <usa.iforvert@gmail.com>
2022-05-17 00:36:21 +0800

    router 使用v6

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -6,7 +6,6 @@ import carouselImage1 from "../../assets/images/carousel_1.jpg"
 import carouselImage2 from "../../assets/images/carousel_2.jpg"
 import carouselImage3 from "../../assets/images/carousel_3.jpg"
 
-
 export const Carousel : React.FC = () => {
     return <AntCarousel autoplay className={styles.slider}>
         <Image src={carouselImage1}/>
